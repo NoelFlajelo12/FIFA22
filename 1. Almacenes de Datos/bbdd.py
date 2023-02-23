0ae39c007a919eb54bb9a2bf33c0e69127ccad85
Vallepablo2000 <91553389+Vallepablo2000@users.noreply.github.com>
2023-02-23 18:21:30 +0100

    Add files via upload

--- 1. Almacenes de Datos/bbdd.py
+++ 1. Almacenes de Datos/bbdd.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import os
+import mysql.connector
+from sqlalchemy import create_engine
 
 ruta=os.getcwd()
 
 df_jugadores = pd.read_csv(ruta +"\\fifa20\\players_20.csv", sep=",")
 df_equipos = pd.read_csv(ruta+"\\fifa20\\teams_and_leagues.csv", sep=",")
 
+print("¿Hay valores nulos?")
 print(df_jugadores.isnull().values.all())
 
 del df_jugadores['sofifa_id']
@@ -37,4 +40,25 @@ del df_jugadores['cb']
 del df_jugadores['rcb']
 del df_jugadores['rb']
 
-print (df_jugadores.head(10))
+
+#conexion con el servidor
+conexion = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+)
+
+#creación del cursor
+cursor = conexion.cursor()
+
+#creación de nueva base de datos
+cursor.execute("DROP DATABASE IF EXISTS FIFA;")
+cursor.execute("CREATE DATABASE FIFA;")
+
+engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
+                       .format(user="root",
+                               pw="root",
+                               db="fifa"))
+
+df_jugadores.to_sql('fifa20', engine, chunksize=500000,
+          method='multi', index=False, if_exists='append')
